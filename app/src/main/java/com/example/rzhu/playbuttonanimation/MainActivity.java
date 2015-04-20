05389a6b6a9d127f085c010f938fa81b9798b06d
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2015-04-20 15:11:31 -0700

    init commit

---
+++ app/src/main/java/com/example/rzhu/playbuttonanimation/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.rzhu.playbuttonanimation;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private View switchButtonIcon;
+    private FrameLayout switchButton;
+    private boolean isPause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchButton = (FrameLayout) findViewById(R.id.btn_switch);
+        switchButtonIcon =  findViewById(R.id.tv_morph);
+        isPause = false;
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPause){
+                    switchButtonIcon.setBackground(getResources().getDrawable(R.drawable.switch_btn_stop_vector));
+                    ((Animatable) switchButtonIcon.getBackground()).start();
+                    switchButton.setBackground(getResources().getDrawable(R.drawable.switch_btn_bg_pause_transition));
+                    TransitionDrawable transition = (TransitionDrawable) switchButton.getBackground();
+                    transition.startTransition(500);
+                    isPause = false;
+                }
+
+                else{
+                    switchButtonIcon.setBackground(getResources().getDrawable(R.drawable.switch_btn_start_vector));
+                    ((Animatable) switchButtonIcon.getBackground()).start();
+                    switchButton.setBackground(getResources().getDrawable(R.drawable.switch_btn_bg_resume_transition));
+                    TransitionDrawable transition = (TransitionDrawable) switchButton.getBackground();
+                    transition.startTransition(500);
+                    isPause = true;
+                }
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
